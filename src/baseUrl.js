208334c41129ad1c64f5cb1dcb52e7382bead239
Rohanwaikar <32079236+Rohanwaikar@users.noreply.github.com>
2025-06-06 15:13:42 +0530

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+let baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 if (baseUrl.endsWith('/')) {
   baseUrl = baseUrl.slice(0, -1);
 }
